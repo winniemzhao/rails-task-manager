3db13d1846ed6df5df497de8ff73956963be9187
Winnie Zhao <winniemaizhao@gmail.com>
2022-08-09 14:51:15 -0400

    refactored code

--- config/routes.rb
+++ config/routes.rb
@@ -4,21 +4,21 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root 'tasks#index'
 
-  # Read all tasks
-  get 'tasks', to: 'tasks#index'
+  # # Read all tasks
+  # get 'tasks', to: 'tasks#index'
 
-  # Add a task
-  get 'tasks/new', to: 'tasks#new', as: :new_task
-  post 'tasks', to: 'tasks#create'
+  # # Add a task
+  # get 'tasks/new', to: 'tasks#new', as: :new_task
+  # post 'tasks', to: 'tasks#create'
 
-  # Read one task
-  get 'tasks/:id', to: 'tasks#show', as: :task
+  # # Read one task
+  # get 'tasks/:id', to: 'tasks#show', as: :task
 
-  # Edit a task
-  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
-  patch 'tasks/:id', to: 'tasks#update'
+  # # Edit a task
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  # patch 'tasks/:id', to: 'tasks#update'
 
-  # Destroy a task
-  delete 'tasks/:id', to: 'tasks#destroy', as: :destroy_task
-  # resources :tasks
+  # # Destroy a task
+  # delete 'tasks/:id', to: 'tasks#destroy'
+  resources :tasks
 end
